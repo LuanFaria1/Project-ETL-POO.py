cff44aab435882d9cf98691513e8a10730235335
LuanFaria1 <farialuan723@gmail.com>
2024-03-31 04:40:02 -0300

    novo commit

--- DataFrameAnalysis.py
+++ DataFrameAnalysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 class DataAnalyzer:
     def __init__(self, data_frame):
@@ -14,6 +15,16 @@ class DataAnalyzer:
         print(self.data_frame.dtypes)
         print("\n4. Análise estatística dos dados:")
         print(self.data_frame.describe().apply(lambda x: round(x, 2)))
+        
+        # Calcula a mediana
+        print("\n5. Mediana:")
+        print(self.data_frame.median())
+        
+        # Calcula o coeficiente de variação para todas as colunas numéricas
+        print("\n6. Coeficiente de Variação:")
+        for column in self.data_frame.select_dtypes(include=['number']).columns:
+            coef_variacao = (self.data_frame[column].std() / self.data_frame[column].mean()) * 100
+            print(f"{column}: {coef_variacao:.2f}%")
 
 class DataVisualizer:
     def __init__(self, data_frame):
@@ -21,7 +32,7 @@ class DataVisualizer:
     
     def plot_distribution(self, column):
         plt.figure(figsize=(8, 6))
-        plt.hist(self.data_frame[column], bins=30, density=True, alpha=0.6, color='g')
+        sns.histplot(self.data_frame[column], kde=True, color='g')
         plt.title(f'Distribuição Normal de {column}')
         plt.xlabel(column)
         plt.ylabel('Densidade')
@@ -30,20 +41,18 @@ class DataVisualizer:
 
     def plot_boxplot(self, column):
         plt.figure(figsize=(6, 4))
-        plt.boxplot(self.data_frame[column])
+        sns.boxplot(x=self.data_frame[column])
         plt.title(f'Boxplot de {column}')
-        plt.ylabel(column)
+        plt.xlabel(column)
         plt.grid(True)
         plt.show()
 
-    def plot_index_vs_time(self, column):
-        tempo = range(len(self.data_frame))
-        indices = self.data_frame[column]
+    def plot_index_vs_time(self, column_tempo, column_indice):
         plt.figure(figsize=(10, 5))
-        plt.plot(tempo, indices, color='b')
-        plt.title(f'Curva {column} vs Tempo')
+        plt.plot(self.data_frame[column_tempo], self.data_frame[column_indice], color='b')
+        plt.title(f'Curva {column_indice} vs Tempo')
         plt.xlabel('Tempo')
-        plt.ylabel(column)
+        plt.ylabel(column_indice)
         plt.grid(True)
         plt.show()
 
@@ -58,6 +67,6 @@ analyzer.analyze()
 visualizer = DataVisualizer(df)
 visualizer.plot_distribution('Total')
 visualizer.plot_boxplot('Total')
-visualizer.plot_index_vs_time('Total')
+visualizer.plot_index_vs_time('Data', 'Total')
 
 print(df)
